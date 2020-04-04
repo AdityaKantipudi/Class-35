bc8b25e282d4616aa0c18fe9e7dc6c2cc1c85fb6
AdityaKantipudi <61592967+AdityaKantipudi@users.noreply.github.com>
2020-04-04 17:52:58 +0530

    Add files via upload

---
+++ JS/Player.js
@@ -0,0 +1,12 @@
+class Player {
+    
+    constructor() {
+
+    }
+    getCount() {
+        var playerCountref = database.ref('playerCount');
+        playerCountref.on("value", function(data){
+            playerCount = data.val();
+        })
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database;
+var canvas, backgroundImage,playerCount;
+var gameState = 0;
+
+function setup(){
+canvas = createCanvas(400,400);
+}
+
+function draw(){
+    background("white");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
